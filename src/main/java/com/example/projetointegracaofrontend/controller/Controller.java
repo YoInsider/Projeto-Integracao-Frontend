58f94073311b92c83d8b4000af992ea311c8c438
YoInsider <insensoscoffee17@proton.me>
2025-06-04 13:20:48 -0300

    Projeto 03

--- src/main/java/com/example/projetointegracaofrontend/controller/Controller.java
+++ src/main/java/com/example/projetointegracaofrontend/controller/Controller.java
@@ -1,15 +1,8 @@
 package com.example.projetointegracaofrontend.controller;
 
-import com.example.projetointegracaofrontend.model.ProductCategoriesDTO;
-import com.example.projetointegracaofrontend.model.ProductLinesDTO;
-import com.example.projetointegracaofrontend.model.ProductModelsDTO;
-import com.example.projetointegracaofrontend.service.ProductCategoriesService;
-import com.example.projetointegracaofrontend.service.ProductLinesService;
-import com.example.projetointegracaofrontend.service.ProductModelsService;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import sun.reflect.generics.tree.Tree;
 
 import java.net.URL;
 import java.util.List;
