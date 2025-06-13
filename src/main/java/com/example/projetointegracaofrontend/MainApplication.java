5104d9f5de08b6d751f4a10247177b8115fd88b2
YoInsider <insensoscoffee17@proton.me>
2025-06-13 09:40:55 -0300

    Correções projeto 03

--- src/main/java/com/example/projetointegracaofrontend/MainApplication.java
+++ src/main/java/com/example/projetointegracaofrontend/MainApplication.java
@@ -2,12 +2,15 @@ package com.example.projetointegracaofrontend;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.application.Application;
 
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
+        Image icon = new javafx.scene.image.Image(getClass().getResourceAsStream("/icon/Projeto-Integracao-Frontend-1.0-SNAPSHOT.png"));
+        stage.getIcons().add(icon);
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Projeto Integração");
